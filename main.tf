16f0bdcbd14d74caf07eb80e6a8844f84616e2e8
naseemsm <naseemsm19@gmail.com>
2020-12-16 22:03:26 -0500

    first plan run

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 terraform {
-  required_providers {
+     backend "remote" {
+       organization = "Samir-Naseem"
+       workspaces {
+    name = "Terraform"
+ }
+ }
+
+required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 2.70"
     }
   }
 }
-
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
@@ -16,3 +22,5 @@ resource "aws_instance" "example" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 }
+
+
